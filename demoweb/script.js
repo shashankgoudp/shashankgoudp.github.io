9a4202b83b23190e8401a2256208a155ea776252
shashank goud <shashankgoudp@gmail.com>
2015-12-24 23:08:56 -0500

    adding all source files

---
+++ demoweb/script.js
@@ -0,0 +1,10 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " by Shashank Goud</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>About Me</a></li>" +
+"<li><a href='about.html'>My Interests</a></li>" +
+"<li><a href='about.html'>PORTFOLIO</a></li>" +
+"<li><a href='about.html'>CONNECT</a></li>" +
+"</ul>";
---
+++ scripts/helper.js
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<title>My Portfolio</title>
+		<link rel="stylesheet" type="text/css" href=".css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="./css/styles.css">
+		<script type="text/javascript" src="./scripts/jQuery.js"></script>
+
+</head>
+
+<body>
+		<div class="container-fluid header-section">
+			<div class="row">
+					<div class="col-md-4">
+						<img src="./img/grad.jpg"  alt="My Image" class="img-responsive">
+
+					</div>
+					<div class="col-md-8"></div>
+
+
+
+			</div>
+
+		</div><!-- division for header section -->
+
+		<div class="container-fluid experience-section">
+		
+		</div><!-- division for experience section -->
+
+		<div class="container-fluid projects-section">
+		
+		</div><!-- division for projects section -->
+
+		<div class="container-fluid skills-section">
+		
+		</div><!-- division for skills section -->
+
+		<div class="container-fluid footer-section">
+		
+		</div><!-- division for footer section -->
+
+		<script type="text/javascript" src="./scripts/jQuery.js"></script>
+</body>
+
+</html>
